fb16c0f8ef9575b296951f41d543162112513417
Pavel Khokhlov <pkhokhlov@hotmail.com>
2015-01-24 11:02:20 -0800

    Robot: implemented a method for mapping instead of mapping everything in teleopPeriodic().
    Signed-off-by: Pavel Khokhlov <pkhokhlov@hotmail.com>

--- src/org/usfirst/frc/team4765/robot/Robot.java
+++ src/org/usfirst/frc/team4765/robot/Robot.java
@@ -2,9 +2,12 @@ package org.usfirst.frc.team4765.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -14,7 +17,8 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 
-public class Robot extends IterativeRobot {
+public class Robot extends IterativeRobot 
+{
 	RobotDrive myRobot;
 	Joystick stick;
 	int autoLoopCounter;
@@ -26,21 +30,34 @@ public class Robot extends IterativeRobot {
 	public final static double JoyKneeOne = 0.1;        // end of the deadzone & first knee of joystick range which starts 'maneuvering range'
     public final static double JoyKneeTwo = 0.8;         // second knee of joystick range which ends 'maneuvering range' and starts 'speed range'
     public final static double JoyMaxRange = 1.0;        // maximum input range of joysticks
+    
+    Preferences prefs = Preferences.getInstance();
+
+    double P;
+    double I;
+    double D;
+    double MAX_RPM;
+    double StartPosition;
+
+    double[] motorSpeed = new double[4]; //holds motor speeds (in rpm)
+
 	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    public void robotInit() 
+    {
         System.out.println("robotInit()");
     	myRobot = new RobotDrive(0,1);
     	stick = new Joystick(0);
-    }
+    }    
     
     /**
      * This function is run once each time the robot enters autonomous mode
      */
-    public void autonomousInit() {
+    public void autonomousInit() 
+    {
     	autoLoopCounter = 0;
     }
    
@@ -65,102 +82,63 @@ public class Robot extends IterativeRobot {
     {
     	
     }
+    
+    /**
+     * This takes the raw input values from the joystick and maps them into more convenient speeds.
+     * There is a deadzone of the first 10% of the joystick range.
+     * From 10% to 80% of the range, the slope of the line is not 1, it is less.
+     * From 80% to 100% of the range the slope is high for driving quickly.
+     * These values are subject to change.
+     * 
+     * TODO: make separate method for R in case we need different knees.
+     */    
+    public double mapValue(double V)
+    {
+    	if (Math.abs(V) < JoyKneeOne) // deadzones
+        {
+            V = 0.0;
+        }
 
+        if ((Math.abs(V) >= JoyKneeOne) && (Math.abs(V) <= JoyKneeTwo)) // mapping for maneuvering range
+        {
+            if (V < 0.0)
+            {
+                V = (3.0 / 7.5) * V - 0.03;     // changes raw negative input into a maneuverable speed
+            } 
+            else
+            {
+                V = (3.0 / 7.5) * V + 0.03;     // changes raw positive input into a maneuverable speed
+            }
+        } 
+        else
+        {
+            if((Math.abs(V) > JoyKneeTwo) && (Math.abs(V) <= JoyMaxRange)) // mapping for speed range
+            {
+                if(V < 0)
+                {
+                    V = (6.5 / 2.0) * V + 2.25;  // changes raw negative input into a fast speed
+                } 
+                else
+                {
+                    V = (6.5 / 2.0) * V - 2.25;  // changes raw positive input into a fast speed
+                }
+            }
+        }        
+        return V;
+    }
+    
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() 
-    {
-    	
+    {    	
     	double Y = driver.getY();
     	double X = driver.getX();
     	double R = driver.getZ();    	
 
-         if (Math.abs(Y) < JoyKneeOne) // deadzones
-         {
-             Y = 0.0;
-         }
-
-         if ((Math.abs(Y) >= JoyKneeOne) && (Math.abs(Y) <= JoyKneeTwo)) // mapping for maneuvering range
-         {
-             if (Y < 0.0)
-             {
-                 Y = (3.0 / 7.5) * Y - 0.03;     // changes raw negative input into a maneuverable speed
-             } 
-             else
-             {
-                 Y = (3.0 / 7.5) * Y + 0.03;     // changes raw positive input into a maneuverable speed
-             }
-         } 
-         else
-         {
-             if((Math.abs(Y) > JoyKneeTwo) && (Math.abs(Y) <= JoyMaxRange)) // mapping for speed range
-             {
-                 if(Y < 0)
-                 {
-                     Y = (6.5 / 2.0) * Y + 2.25;  // changes raw negative input into a fast speed
-                 } 
-                 else
-                 {
-                     Y = (6.5 / 2.0) * Y - 2.25;  // changes raw positive input into a fast speed
-                 }
-             }
-         }
-         
-         
-         if ((Math.abs(X) >= JoyKneeOne) && (Math.abs(X) <= JoyKneeTwo)) // mapping for maneuvering range
-         {
-             if (X < 0.0)
-             {
-                 X = (3.0 / 7.5) * X - 0.03;     // changes raw negative input into a maneuverable speed
-             } 
-             else
-             {
-                 X = (3.0 / 7.5) * X + 0.03;     // changes raw positive input into a maneuverable speed
-             }
-         } 
-         else
-         {
-             if((Math.abs(X) > JoyKneeTwo) && (Math.abs(X) <= JoyMaxRange)) // mapping for speed range
-             {
-                 if(X < 0)
-                 {
-                     X = (6.5 / 2.0) * X + 2.25;  // changes raw negative input into a fast speed
-                 } 
-                 else
-                 {
-                     X = (6.5 / 2.0) * X - 2.25;  // changes raw positive input into a fast speed
-                 }
-             }
-         }
-         
-         if ((Math.abs(R) >= JoyKneeOne) && (Math.abs(R) <= JoyKneeTwo)) // mapping for maneuvering range
-         {
-             if (R < 0.0)
-             {
-                 R = (3.0 / 7.5) * R - 0.03;     // changes raw negative input into a maneuverable speed
-             } 
-             else
-             {
-                 R = (3.0 / 7.5) * R + 0.03;     // changes raw positive input into a maneuverable speed
-             }
-         } 
-         else
-         {
-             if((Math.abs(R) > JoyKneeTwo) && (Math.abs(R) <= JoyMaxRange)) // mapping for speed range
-             {
-                 if(R < 0)
-                 {
-                     R = (6.5 / 2.0) * R + 2.25;  // changes raw negative input into a fast speed
-                 } 
-                 else
-                 {
-                     R = (6.5 / 2.0) * R - 2.25;  // changes raw positive input into a fast speed
-                 }
-             }
-         }
-
-
+        Y = mapValue(Y);
+        X = mapValue(X);
+        R = mapValue(R);
     	
     	double motor1speed = X + Y + -0.5 * R;
     	double motor2speed = -1.0 * X + Y + 0.5 * R;
@@ -178,19 +156,15 @@ public class Robot extends IterativeRobot {
     	motor1.set(-1.0 * motor1speed);
     	motor2.set(motor2speed);
     	motor3.set(motor3speed);
-    	
-    	
-    	
-    	
-    	// TODO: make another method for controlling and call it in teleop for organization
-    		
-        myRobot.arcadeDrive(stick);
+        		
+        //myRobot.arcadeDrive(stick);
     }
     
     /**
      * This function is called periodically during test mode
      */
-    public void testPeriodic() {
+    public void testPeriodic() 
+    {
     	LiveWindow.run();
     }
     
